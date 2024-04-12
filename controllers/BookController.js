3a9f24f4e52882a32073fa49fb2b75eaac65dc27
SSOFERRET <owosso1212@gmail.com>
2024-04-12 14:16:54 +0900

    refact : 도서 조회 API 리팩토링 업데이트

--- controllers/BookController.js
+++ controllers/BookController.js
@@ -1,16 +1,37 @@
 const conn = require('./../mariadb');
 const {StatusCodes} = require('http-status-codes');
 const dotenv = require('dotenv');
-
+//[2기] 박소현
 const allBooks = (req, res) => {
-    let sql = "SELECT * FROM books";
-    conn.query(sql,
+    let {category_id, news, limit, page} = req.query;
+    category_id = +category_id;
+    limit = +limit;
+    page = +page;
+    const offsetValue = (page-1)*limit;
+    const values = [limit, offsetValue];
+    
+    let sql = `select * from books left join BookShop.category 
+                on BookShop.books.category_id = BookShop.category.id `;
+    if (category_id || news) 
+        sql += " where "
+    if (category_id) {
+        sql += "category_id = ?";
+        values.unshift(category_id);
+    }
+    if (category_id && news)
+        sql += " and "
+    if (news)
+        sql += "pub_date between date_sub(now(), interval 1 month) and now() ";
+
+    sql += " limit ? offset ?";
+
+    conn.query(sql, values,
         (err, results) => {
         if(err) {
             console.log(err);
             return res.status(StatusCodes.BAD_REQUEST).end();
         }
-      
+        
         res.status(StatusCodes.OK).json({results});
     });
 };
@@ -18,7 +39,9 @@ const allBooks = (req, res) => {
 const bookDetail = (req, res) => {
     let {id} = req.params;
 
-    let sql = "SELECT * FROM books WHERE id = ?";
+    let sql = `select * from BookShop.books left
+                join BookShop.category on BookShop.books.category_id = BookShop.category.id
+                where BookShop.books.id = ?`;
     conn.query(sql, id,
         (err, results) => {
         if(err) {
@@ -33,7 +56,7 @@ const bookDetail = (req, res) => {
 };
 
 const booksByCategory = (req, res) => {
-    let {category_id} = req.query;
+    let {category_id, news} = req.query;
     console.log(req.query);
 
     let sql = "SELECT * FROM books WHERE category_id = ?";
@@ -50,12 +73,33 @@ const booksByCategory = (req, res) => {
     });
 };
 
+const newBooksByCategory = (req, res) => {
+    let {category_id, news} = req.query;
+    console.log(req.query);
+
+    let sql = "SELECT * FROM books WHERE category_id = ?";
+    conn.query(sql, category_id,
+        (err, results) => {
+        if(err) {
+            console.log(err);
+            return res.status(StatusCodes.BAD_REQUEST).end();
+        }
+        if (results.length)
+            return res.status(StatusCodes.OK).json(results);
+        else
+            return res.status(StatusCodes.NOT_FOUND).end();
+    });
+};
+//[2기 박소현]
 const booksSearch = (req, res) => {
     console.log(req.query)
     if (req.query.category_id) 
         booksByCategory(req, res);
-    else 
-        allBooks(req, res); 
+    else if (req.query.category_id && req.query.news)
+        newBooksByCategory(req, res);
+    // else if ()
+    // else 
+    //     allBooks(req, res); 
 }
 
 module.exports = {
